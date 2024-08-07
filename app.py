db805fd78411e86fe99f277d79b5d8c501fb48c1
manchov <manchov@outlook.com>
2024-08-07 20:58:51 +0200

    root app.py made as the now former web/app.py >:(

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
+import os
+from flask import Flask, render_template, jsonify
+from database import service_db
+# from web import create_app
 from flask import Flask
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_object('config.Config')
+
+    # db.init_app(app)
+
+    with app.app_context():
+        # Import parts of our application
+        # from . import routes  # This should ensure routes are registered
+        from web import routes
+
+        # Print registered routes for debugging
+        print(app.url_map)
+
+        # Create database tables for our data models
+        # db.create_all()
+
+    return app
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+application = create_app()
+app = application
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
