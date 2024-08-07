d9623ad1cd7e9bc20507fc35186d6b2591d02fc6
manchov <manchov@outlook.com>
2024-08-07 16:47:39 +0200

    config.py removed function

--- web/config.py
+++ web/config.py
@@ -1,9 +1,10 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../database/bulletins.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# import os
+#
+# basedir = os.path.abspath(os.path.dirname(__file__))
+#
+#
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../database/bulletins.db')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+print("config opened")
