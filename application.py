96464979d97969f031e1035697010d72e42ba7c4
manchov <manchov@outlook.com>
2024-08-07 17:19:32 +0200

    edited #2 application.py just for testing

--- application.py
+++ application.py
@@ -1,12 +1,35 @@
 from flask import Flask
 
-application = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    # db.init_app(app)
+
+    # with app.app_context():
+    #     # Import parts of our application
+    #     # from . import routes  # This should ensure routes are registered
+    #     # import routes
+    #
+    #     # Print registered routes for debugging
+    #     # print(app.url_map)
+    #
+    #     # Create database tables for our data models
+    #     # db.create_all()
+
+    return app
 
 
-@application.route('/')
+application = create_app()
+
+app = application
+
+
+@app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
