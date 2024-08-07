8acce9cadd5d3afe0a46d858a6dad43c24dbc1e0
manchov <manchov@outlook.com>
2024-08-07 17:34:16 +0200

    web/app.py made for main call on WSGI

--- web/app.py
+++ web/app.py
@@ -25,51 +25,9 @@ def create_app():
     return app
 
 
-# db = SQLAlchemy()
-# app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object('config.Config')
-#
-#     db.init_app(app)
-#
-#     with app.app_context():
-#         from . import routes, models
-#         db.create_all()
-#
-#     return app
-
-app = create_app()
-#
-# def get_bulletins():
-#     bulletins = service_db.get_database("SELECT date, description, location, latitude, longitude FROM bulletins")
-#
-#     # Convert the result to a list of dictionaries
-#     bulletins_list = []
-#     for row in bulletins:
-#         bulletins_list.append({
-#             'date': row[0],
-#             'description': row[1],
-#             'location': row[2],
-#             'latitude': row[3],
-#             'longitude': row[4]
-#         })
-#
-#     return bulletins_list
-
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-#
-# @app.route('/api/bulletins')
-# def api_bulletins():
-#     bulletins = get_bulletins()
-#     return jsonify(bulletins)
-
-
+application = create_app()
+app = application
 if __name__ == '__main__':
     app.run(debug=True)
